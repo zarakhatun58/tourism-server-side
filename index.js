2d8c5c6d71e9f500f3f8f560d58f6e6e37f5d2b5
jahanara khatun <zarakhatun58@gmail.com>
2021-11-23 00:22:25 +0530

    added

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ async function run() {
             res.send(product);
         });
 
-         // cancel an Booking
+         // cancel an Booking ////
         app.delete('/Bookings/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
